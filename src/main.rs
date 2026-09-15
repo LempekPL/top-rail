9f43b0b85db5d44baa33a3562c09225386b000b0
Lempek <37885098+LempekPL@users.noreply.github.com>
2026-09-15 14:18:08 +0200

    update framerate to lower latency

--- src/main.rs
+++ src/main.rs
@@ -6,33 +6,47 @@ mod util;
 
 use crate::camera::CameraPlugin;
 use crate::railway::RailwayPlugin;
-// use bevy::input::common_conditions::input_toggle_active;
-use bevy::prelude::*;
-// use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
 use bevy::dev_tools::fps_overlay::{FpsOverlayConfig, FpsOverlayPlugin, FrameTimeGraphConfig};
+use bevy::prelude::*;
+use bevy::window::PresentMode;
+use bevy_framepace::{FramepacePlugin, FramepaceSettings, Limiter};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugins(FpsOverlayPlugin { config: FpsOverlayConfig {
-            text_config: TextFont {
-                font_size: FontSize::Px(10.0),
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                present_mode: PresentMode::Mailbox,
                 ..default()
+            }),
+            ..default()
+        }))
+        .add_plugins(FpsOverlayPlugin {
+            config: FpsOverlayConfig {
+                text_config: TextFont {
+                    font_size: FontSize::Px(10.0),
+                    ..default()
+                },
+                text_color: Color::srgb(0.0, 1.0, 0.0),
+                enabled: true,
+                refresh_interval: core::time::Duration::from_millis(100),
+                frame_time_graph_config: FrameTimeGraphConfig {
+                    enabled: false,
+                    min_fps: 30.0,
+                    target_fps: 144.0,
+                },
             },
-            text_color: Color::srgb(0.0, 1.0, 0.0),
-            enabled: true,
-            refresh_interval: core::time::Duration::from_millis(100),
-            frame_time_graph_config: FrameTimeGraphConfig {
-                enabled: false,
-                min_fps: 30.0,
-                target_fps: 144.0,
-            },
-        } })
+        })
         .add_plugins(CameraPlugin)
         .add_plugins(RailwayPlugin)
+        .add_plugins(FramepacePlugin)
+        .add_systems(Startup, setup_framerate)
         // .add_plugins(EguiPlugin::default())
         // .add_plugins(
         //     WorldInspectorPlugin::default().run_if(input_toggle_active(true, KeyCode::Escape)),
         // )
         .run();
+}
+
+fn setup_framerate(mut settings: ResMut<FramepaceSettings>) {
+    settings.limiter = Limiter::from_framerate(165.0);
 }
